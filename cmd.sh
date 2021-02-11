880c000fab6473204486fcf8b8c1859a1ba8bb21
Cyd <34154246+cyd01@users.noreply.github.com>
2021-02-11 11:35:25 +0100

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 if [ $REQUEST_METHOD == "GET" ]; then
+  echo "Status: 200"
+  echo "Content-type: text/html"
+  echo
   env | sort
 else
 
   if [ $HTTP_CONTENT_LENGTH -gt 0 ]; then
+    echo "Status: 200"
+    echo "Content-type: text/html"
+    echo
     echo "Here is the body:"
     #read -n $HTTP_CONTENT_LENGTH body
     #echo $body
     cat
   else
+    echo "Status: 200"
+    echo "Content-type: text/html"
+    echo
     echo "There is no body"
   fi
 fi
